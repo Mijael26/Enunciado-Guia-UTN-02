ab22f60cbcd62bfdc91bd00de44094bb3ac8c698
Mijael Cuenca <90813944+Mijael26@users.noreply.github.com>
2022-01-18 19:02:20 -0300

    Add files via upload

---
+++ Ejercicio 4 .py
@@ -0,0 +1,24 @@
+nota = [0] * (5)
+legajo = [0] * (5)
+
+sum = 0
+pRO = 0
+cont = 0
+for h in range(0, 5, 1):
+    nota[h] = int(input("Ingrese nota : "))
+    legajo[h] = int(input("Ingrese Legajo : "))
+    sum = sum + nota[h]
+
+pRO = float(sum) / 5
+
+for h in range(0, 5, 1):
+    if pRO >= nota[h]:
+        cont = cont + 1
+
+print("Los legajos con notas mayores que 4  : ")
+for h in range (0,5,1) :
+    if nota[h]>4 :
+        print(legajo[h])
+
+print(f"Cuantos sacaron mas que el promedio : {cont}")
+
